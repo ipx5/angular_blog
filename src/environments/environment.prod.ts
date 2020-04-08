a79c8422b8a2a97f00cfd0f974d6ad386a6550e4
Ilya.Polyakov <Ilya.Polyakov@x5.ru>
2020-04-08 15:28:09 +0300

    without_pwa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyA9ZUiM4tZ6W4Zab7duB-myf_g-gBNlUug',
+  fbDbUrl: 'https://angular-blog-9d0aa.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyA9ZUiM4tZ6W4Zab7duB-myf_g-gBNlUug',
+  fbDbUrl: 'https://angular-blog-9d0aa.firebaseio.com'
 };
 
 /*
