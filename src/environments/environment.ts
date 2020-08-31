ef0bd48534b20542f98a4a7b7c3540cfc8cfd774
ipx5 <polyakovx5@gmail.com>
2020-08-31 09:36:42 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyA9ZUiM4tZ6W4Zab7duB-myf_g-gBNlUug',
-  fbDbUrl: 'https://angular-blog-9d0aa.firebaseio.com'
+  apiKey: '====',
+  fbDbUrl: '===='
 };
 
 /*
